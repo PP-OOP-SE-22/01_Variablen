d6a1594b46bfa8ff11e243a19e4c8eb9b2686eea
Sabine.Eitel <Seit201@aol.com>
2022-11-08 12:14:55 +0100

    Kommentar strg#

--- script_01.js
+++ script_01.js
@@ -10,3 +10,21 @@
 
 // Konsolen-Ausgaben
 // console.log("Hallo");
+// console.log(firstName);
+
+/***** 02 Deklaration + Wertzuweisung I *******/
+
+let firstName; // Deklaration (Definition)
+firstName ="Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+
+
+
+
+
+
+
