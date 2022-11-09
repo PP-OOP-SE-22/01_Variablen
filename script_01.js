c16ac3895f2f178061bf44f1360247c9a8d655af
Sabine.Eitel <Seit201@aol.com>
2022-11-09 09:31:42 +0100

    Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -14,17 +14,27 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName ="Max"; // Wertzuweisung
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
 // console.log(firstName); // Ausgabe
 
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
 // console.log(familyName); // Ausgabe
-console.log(firstName + " " + familyName);
+// console.log(firstName + " " + familyName);
 
+/***** 03 Deklaration + Wertzuweisung II *******/
 
+// let firstName, familyName;
+// firstName = prompt("Bitte Vornamen eingeben:"); 
+// familyName = prompt("Bitte Namen eingeben:"); 
+// console.log(firstName + " " + familyName);
 
+/* JS ist eine untypisierte Sprache! | untyped */
 
+let test;
+test = "Hi";
+test = 2;
+test = true;
 
-
-
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test); // Ausgabe
